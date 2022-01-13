190ee41292a74fd529b72260f6c917403d992af1
Carmen Martín Núñez <carmen.martin@iesnervion.es>
2022-01-13 10:42:01 +0100

    commit 2

--- src/principal/Cubo.java
+++ src/principal/Cubo.java
@@ -75,4 +75,59 @@ public class Cubo {
     public void setCapcidadMaxima (double capcidadMaxima){
         this.capcidadMaxima = capcidadMaxima;
     }
+
+    //constructor copia
+
+    public Cubo (Cubo cubo){
+
+        this.material = cubo.getMaterial();
+        this.color = cubo.getColor();
+        this.asa = cubo.getAsa();
+        this.contenido = cubo.getContenido();
+        this.capcidadMaxima = cubo.getCapcidadMaxima();
+
+    }
+
+    //metodo para llenar el cubo
+
+    public void llenarCubo (){
+
+        contenido+=capcidadMaxima;
+
+        if (capcidadMaxima==contenido){
+            System.out.println("El cubo está lleno a su máxima capacidad");
+        }else if (capcidadMaxima>contenido){
+            System.out.println("El cubo puede llenarse más");
+        }
+
+    }
+
+    //metodo para vaciar el cubo
+
+    public void vaciarCubo (int cantidad){
+
+        contenido-=cantidad;
+
+        if (contenido == 0) {
+            System.out.println("El cubo está vacío");
+        }
+
+    }
+
+    //metodo para volcar agua de un cubo a otro
+
+    public void volcarContenido (int cantidadVolcar, Cubo cubo){
+
+        cubo.contenido+=cantidadVolcar;
+        this.contenido-=cantidadVolcar;
+
+        if (capcidadMaxima < cubo.contenido){
+            System.out.println("El agua del cubo ha rebosado");
+        }else{
+
+        }
+
+    }
+
+    //metodo para dibujar cubo
 }
--- src/principal/Main.java
+++ src/principal/Main.java
@@ -3,6 +3,11 @@ package principal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Cubo cubo1 = new Cubo("plástico", "azul", true, 20, 50);
+
+        System.out.println("El cubo tiene:" + cubo1.getContenido() + "cl");
+        cubo1.llenarCubo();
+
     }
 }
